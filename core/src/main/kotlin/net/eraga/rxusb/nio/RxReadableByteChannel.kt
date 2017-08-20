ab0c50b6171f7b4a20a6fa493936c1453c05490c
Klaus Schwartz <tntclaus@gmail.com>
2017-08-20 08:46:32 +0300

    fix logging

--- core/src/main/kotlin/net/eraga/rxusb/nio/RxReadableByteChannel.kt
+++ core/src/main/kotlin/net/eraga/rxusb/nio/RxReadableByteChannel.kt
@@ -3,7 +3,6 @@ package net.eraga.rxusb.nio
 import io.reactivex.Flowable
 import org.reactivestreams.Subscriber
 import org.reactivestreams.Subscription
-import org.slf4j.LoggerFactory
 import java.nio.ByteBuffer
 import java.nio.channels.ReadableByteChannel
 import java.util.concurrent.atomic.AtomicLong
@@ -15,34 +14,23 @@ import java.util.concurrent.atomic.AtomicLong
 abstract class RxReadableByteChannel(
         var byteBufferSize: Int = 1024 * 4
 ) : Flowable<ByteArray>(), ReadableByteChannel {
-    private val log = LoggerFactory.getLogger(RxReadableByteChannel::class.java)
 
     class ReadBytesSubscription(
             val subscriber: Subscriber<in ByteArray>,
             val channel: RxReadableByteChannel,
-            byteBufferSize: Int = 1024 * 4) : Subscription {
-
-        private val log = LoggerFactory.getLogger(ReadBytesSubscription::class.java)
+            byteBufferSize: Int) : Subscription {
 
 
         var buffer: ByteBuffer = ByteBuffer.allocateDirect(byteBufferSize)
 
         init {
-            log.info("init {}", channel.isSubscriptionActive)
             channel.isSubscriptionActive = true
-            log.info("init {}", channel.isSubscriptionActive)
-//            private AtomicLong outStandingRequests = new AtomicLong(0);
         }
 
         override fun cancel() {
-
-            log.info("cancel {}", channel.isSubscriptionActive)
             channel.isSubscriptionActive = false
-            log.info("cancel {}", channel.isSubscriptionActive)
             subscriber.onComplete()
         }
-
-        val lock: Any = Any()
 
         @Volatile private var isProducing = false
         private val outStandingRequests = AtomicLong(0)
@@ -53,13 +41,12 @@ abstract class RxReadableByteChannel(
                 return
 
             channel.isSubscriptionActive = true
-//            log.info("request {}", n)
 
-            outStandingRequests.addAndGet(n);
+            outStandingRequests.addAndGet(n)
 
             while (channel.isOpen && outStandingRequests.get() > 0) {
                 if (isProducing) {
-                    return;
+                    return
                 }
                 // start producing
                 isProducing = true
@@ -71,20 +58,18 @@ abstract class RxReadableByteChannel(
                 buffer.clear()
 
                 subscriber.onNext(array)
-                outStandingRequests.decrementAndGet();
+                outStandingRequests.decrementAndGet()
 
-                isProducing = false;
+                isProducing = false
             }
         }
 
     }
 
-    lateinit var subscription: Subscription
+    private lateinit var subscription: Subscription
 
     override fun subscribeActual(subscriber: Subscriber<in ByteArray>) {
-        log.info("subscribeActual {}", isSubscriptionActive)
         if (!isSubscriptionActive) {
-            log.info("subscribeActual {}", isSubscriptionActive)
             subscription = ReadBytesSubscription(subscriber, this, byteBufferSize)
         }
 
@@ -100,8 +85,6 @@ abstract class RxReadableByteChannel(
     @Synchronized
     override fun close() {
         subscription.cancel()
-
-
     }
 
 
